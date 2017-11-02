1e4a894be06796571ab62720551df78b19012929
Andrea Zerio <andreazerio@Andreas-MacBook-Pro.local>
2017-11-02 09:39:15 +0000

    adding tags route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router(); 
 const threadRouter = require('./threadRoutes'); 
 const quizzesRouter = require('./quizzesRoutes'); 
+const tagsRouter = require('./tagsRouter')
 
 
 
 router.use('/threads', threadRouter);
 router.use('/quizzes', quizzesRouter);
+router.use('/tags', tagsRouter);
 
 
 module.exports=Router 
---
+++ routes/tagsRouter.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const tagsCtrl = require('../controllers/tagsCtrl')
+
+router.post('/',tagsCtrl)
