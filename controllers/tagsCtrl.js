25e99296711dadfbd6b125fd64ea6c6f2635fc8b
Johnny Dunsby <jmdunsby@gmail.com>
2017-11-16 15:02:37 +0000

    Alter require path to config folder in index.js

--- controllers/tagsCtrl.js
+++ controllers/tagsCtrl.js
@@ -1,9 +1,8 @@
 const postTag = require('../models/tags');
 
-
-
 const addTag = (req, res) => {
     const body = req.body;
+    console.log('BODY', body);
     postTag(body)
     .then(newTag => res.send(newTag) )
 };
--- models/tags.js
+++ models/tags.js
@@ -1,7 +1,12 @@
 const db = require('../');
 
 const postTag = (tag) => {
+    console.log('Tag', tag);
     return db.one('INSERT INTO tags (name, type, detail) VALUES ($1, $2, $3) RETURNING *;'[tag.name, tag.type, tag.detail])
+    .then(res => {
+        console.log('QUERY END');
+    })
+    .catch(console.error);
 }
 
 module.exports = postTag
