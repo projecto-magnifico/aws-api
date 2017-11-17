caee87279f6f7e8af112ef11d0f8a0a036fb2ae3
Johnny Dunsby <jmdunsby@gmail.com>
2017-11-17 10:07:23 +0000

    Add comma to tags query

--- models/tags.js
+++ models/tags.js
@@ -1,10 +1,7 @@
 const db = require('../');
 
 const postTag = (tag) => {
-    console.log('TagName', tag.name);
-    console.log('TagType', tag.type);
-    console.log('TagDetail', tag.detail);
-    return db.one('INSERT INTO tags (name, type, detail) VALUES ($1, $2, $3);'[tag.name, tag.type, tag.detail])
+    return db.one('INSERT INTO tags (name, type, detail) VALUES ($1, $2, $3);', [tag.name, tag.type, tag.detail])
     .then(res => {
         console.log('QUERY END');
     })
