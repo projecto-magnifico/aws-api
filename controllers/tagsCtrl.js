9e518c26702af3d403cad7daf6de71862f4971af
Johnny Dunsby <jmdunsby@gmail.com>
2017-11-17 10:01:02 +0000

    Add console logs of tag properties

--- controllers/tagsCtrl.js
+++ controllers/tagsCtrl.js
@@ -2,7 +2,6 @@ const postTag = require('../models/tags');
 
 const addTag = (req, res) => {
     const body = req.body;
-    console.log('BODY', body);
     postTag(body)
     .then(newTag => res.send(newTag) )
 };
--- models/tags.js
+++ models/tags.js
@@ -1,7 +1,9 @@
 const db = require('../');
 
 const postTag = (tag) => {
-    console.log('Tag', tag);
+    console.log('TagName', tag.name);
+    console.log('TagType', tag.type);
+    console.log('TagDetail', tag.detail);
     return db.one('INSERT INTO tags (name, type, detail) VALUES ($1, $2, $3);'[tag.name, tag.type, tag.detail])
     .then(res => {
         console.log('QUERY END');
